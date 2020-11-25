2685923fb7da59e8a5c99e9a60bbb592bed77b3b
shafrazi <shafrazi@live.com>
2020-11-25 16:08:16 +0530

    add react router

--- app/javascript/packs/App.js
+++ app/javascript/packs/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Cell from "./components/Cell";
 import PopUpBox from "./components/PopUpBox";
